7f1e17bc6d998572adf706ec62b66baf731e2c52
BrunoBatalha <brunobatalha37@gmail.com>
2020-12-25 15:09:02 -0400

    use function recursive

--- js/index.js
+++ js/index.js
@@ -18,39 +18,49 @@ function gerenciaContador() {
     new Date(+window.location.search.replace("?t=", "")).getTime() / 1000;
   // const segundosFinal =
   //   new Date(+window.localStorage.getItem("timestamp")).getTime() / 1000;
-  setInterval(() => {
-    const segundosAgora = new Date().getTime() / 1000;
+  contador();
+  function contador() {
+    setTimeout(() => {
+      const segundosAgora = new Date().getTime() / 1000;
 
-    const eSegundos = document.querySelector(".segundos>p");
-    const eMinutos = document.querySelector(".minutos>p");
-    const eHoras = document.querySelector(".horas>p");
-    const eDias = document.querySelector(".dias>p");
+      const eSegundos = document.querySelector(".segundos>p");
+      const eMinutos = document.querySelector(".minutos>p");
+      const eHoras = document.querySelector(".horas>p");
+      const eDias = document.querySelector(".dias>p");
 
-    const diferencaSegundos = segundosFinal - segundosAgora;
+      const diferencaSegundos = segundosFinal - segundosAgora;
 
-    const dias = Math.floor(diferencaSegundos / 60 / 60 / 24);
-    const horas = Math.floor(diferencaSegundos / 60 / 60);
-    const minutos = Math.floor(diferencaSegundos / 60);
-    const segundos = Math.floor(diferencaSegundos);
+      const dias = Math.floor(diferencaSegundos / 60 / 60 / 24);
+      const horas = Math.floor(diferencaSegundos / 60 / 60);
+      const minutos = Math.floor(diferencaSegundos / 60);
+      const segundos = Math.floor(diferencaSegundos);
 
-    if (dias < 0 && horas < 0 && minutos < 0 && segundos < 0 && !alertouFinal) {
-      alertouFinal = true;
-      eSegundos.classList.remove("brilha");
-      const h2 = document.querySelector("h2");
-      h2.innerHTML = "Fim";
-      h2.style.color = "red";
-    }
-    if (!alertouFinal) {
-      eDias.innerHTML = dias < 0 ? "00" : dias.toString().padStart(2, "0");
-      eHoras.innerHTML = (horas - 24 * dias).toString().padStart(2, "0");
-      eMinutos.innerHTML = (minutos - 60 * horas).toString().padStart(2, "0");
-      eSegundos.innerHTML = (segundos - 60 * minutos)
-        .toString()
-        .padStart(2, "0");
-      if (dias <= 0 && horas <= 0 && minutos <= 0)
-        eSegundos.classList.add("brilha");
-    }
-  }, 1000);
+      if (
+        dias < 0 &&
+        horas < 0 &&
+        minutos < 0 &&
+        segundos < 0 &&
+        !alertouFinal
+      ) {
+        alertouFinal = true;
+        eSegundos.classList.remove("brilha");
+        const h2 = document.querySelector("h2");
+        h2.innerHTML = "Fim";
+        h2.style.color = "red";
+      }
+      if (!alertouFinal) {
+        eDias.innerHTML = dias < 0 ? "00" : dias.toString().padStart(2, "0");
+        eHoras.innerHTML = (horas - 24 * dias).toString().padStart(2, "0");
+        eMinutos.innerHTML = (minutos - 60 * horas).toString().padStart(2, "0");
+        eSegundos.innerHTML = (segundos - 60 * minutos)
+          .toString()
+          .padStart(2, "0");
+        if (dias <= 0 && horas <= 0 && minutos <= 0)
+          eSegundos.classList.add("brilha");
+      }
+      contador();
+    }, 1000);
+  }
 }
 
 function init() {
@@ -61,6 +71,7 @@ function init() {
   btnReiniciar && btnReiniciar.addEventListener("click", voltarParaForm);
 
   const tempo = document.querySelector(".tempo");
-  if (tempo) gerenciaContador();
+  tempo && gerenciaContador();
 }
+
 init();
