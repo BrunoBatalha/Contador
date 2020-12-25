f7888fdcda5d7bf727f6a0127d2bf79d52cb67f3
BrunoBatalha <brunobatalha37@gmail.com>
2020-12-25 14:39:53 -0400

    modularizes functions and passes timestamp to contador.html as queryString

--- js/index.js
+++ js/index.js
@@ -1,63 +1,66 @@
 "use strict";
+
+function salvaTimestamp(e) {
+  e.preventDefault();
+  const input = e.target[0];
+  const datahora = new Date(input.value);
+  window.localStorage.setItem("timestamp", datahora.getTime());
+  window.location = "/contador.html?t=" + datahora.getTime();
+}
+
+function voltarParaForm() {
+  window.history.back();
+}
+
+function gerenciaContador() {
+  let alertouFinal = false;
+  const segundosFinal =
+    new Date(+window.location.search.replace("?t=", "")).getTime() / 1000;
+  // const segundosFinal =
+  //   new Date(+window.localStorage.getItem("timestamp")).getTime() / 1000;
+  setInterval(() => {
+    const segundosAgora = new Date().getTime() / 1000;
+
+    const eSegundos = document.querySelector(".segundos>p");
+    const eMinutos = document.querySelector(".minutos>p");
+    const eHoras = document.querySelector(".horas>p");
+    const eDias = document.querySelector(".dias>p");
+
+    const diferencaSegundos = segundosFinal - segundosAgora;
+
+    const dias = Math.floor(diferencaSegundos / 60 / 60 / 24);
+    const horas = Math.floor(diferencaSegundos / 60 / 60);
+    const minutos = Math.floor(diferencaSegundos / 60);
+    const segundos = Math.floor(diferencaSegundos);
+
+    if (dias < 0 && horas < 0 && minutos < 0 && segundos < 0 && !alertouFinal) {
+      alertouFinal = true;
+      eSegundos.classList.remove("brilha");
+      const h2 = document.querySelector("h2");
+      h2.innerHTML = "Fim";
+      h2.style.color = "red";
+    }
+    if (!alertouFinal) {
+      eDias.innerHTML = dias < 0 ? "00" : dias.toString().padStart(2, "0");
+      eHoras.innerHTML = (horas - 24 * dias).toString().padStart(2, "0");
+      eMinutos.innerHTML = (minutos - 60 * horas).toString().padStart(2, "0");
+      eSegundos.innerHTML = (segundos - 60 * minutos)
+        .toString()
+        .padStart(2, "0");
+      if (dias <= 0 && horas <= 0 && minutos <= 0)
+        eSegundos.classList.add("brilha");
+    }
+  }, 1000);
+}
+
 function init() {
   const form = document.querySelector("form");
-  form &&
-    form.addEventListener("submit", (e) => {
-      e.preventDefault();
-      const input = e.target[0];
-      const datahora = new Date(input.value);
-      window.localStorage.setItem("timestamp", datahora.getTime());
-      window.location = "/contador.html";
-    });
+  form && form.addEventListener("submit", salvaTimestamp);
+
   const btnReiniciar = document.querySelector("#btnReiniciar");
-  btnReiniciar &&
-    btnReiniciar.addEventListener("click", (e) => {
-      window.history.back();
-    });
+  btnReiniciar && btnReiniciar.addEventListener("click", voltarParaForm);
+
   const tempo = document.querySelector(".tempo");
-  if (tempo) {
-    let alertouFinal = false;
-    const segundosFinal =
-      new Date(+window.localStorage.getItem("timestamp")).getTime() / 1000;
-    setInterval(() => {
-      const segundosAgora = new Date().getTime() / 1000;
-
-      const eSegundos = document.querySelector(".segundos>p");
-      const eMinutos = document.querySelector(".minutos>p");
-      const eHoras = document.querySelector(".horas>p");
-      const eDias = document.querySelector(".dias>p");
-
-      const diferencaSegundos = segundosFinal - segundosAgora;
-
-      const dias = Math.floor(diferencaSegundos / 60 / 60 / 24);
-      const horas = Math.floor(diferencaSegundos / 60 / 60);
-      const minutos = Math.floor(diferencaSegundos / 60);
-      const segundos = Math.floor(diferencaSegundos);
-
-      if (
-        dias < 0 &&
-        horas < 0 &&
-        minutos < 0 &&
-        segundos < 0 &&
-        !alertouFinal
-      ) {
-        alertouFinal = true;
-        eSegundos.classList.remove("brilha");
-        const h2 = document.querySelector("h2");
-        h2.innerHTML = "Fim";
-        h2.style.color = "red";
-      }
-      if (!alertouFinal) {
-        eDias.innerHTML = dias < 0 ? "00" : dias.toString().padStart(2, "0");
-        eHoras.innerHTML = (horas - 24 * dias).toString().padStart(2, "0");
-        eMinutos.innerHTML = (minutos - 60 * horas).toString().padStart(2, "0");
-        eSegundos.innerHTML = (segundos - 60 * minutos)
-          .toString()
-          .padStart(2, "0");
-        if (dias <= 0 && horas <= 0 && minutos <= 0)
-          eSegundos.classList.add("brilha");
-      }
-    }, 1000);
-  }
+  tempo && gerenciaContador();
 }
 init();
